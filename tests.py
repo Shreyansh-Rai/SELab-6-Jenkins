b23eb551cbcaa24c07793320ce845a9dab454320
Genkai <shreyanshrajeevarai@gmail.com>
2022-11-24 23:02:00 +0530

    Changes made to code 2

--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@
 import unittest
 
 from code import add
+from code2 import divide
+
 
 class TestSum(unittest.TestCase):
 	def test_1(self):
@@ -10,7 +12,7 @@ class TestSum(unittest.TestCase):
 		v = 0
 		result = add(u, v)
 		self.assertEqual(result, 1)
-		
+
 	def test_2(self):
 		u = 30
 		v = 10
@@ -26,14 +28,32 @@ class TestSum(unittest.TestCase):
 	def test_4(self):
 		u = "Hello "
 		v = "World"
-		result = add(u,v)
-		self.assertEqual(result, "Hello World") 
+		result = add(u, v)
+		self.assertEqual(result, "Hello World")
 
 	def test_5(self):
 		u = 0.60
 		v = 0.09
-		result = add(u,v)
+		result = add(u, v)
 		self.assertEqual(result, 0.69)
+
+	def test_6(self):
+		u = 100
+		v = 0
+		result = divide(u,v)
+		self.assertEqual(result,-1)
+
+	def test_7(self):
+		u = 100
+		v = 50
+		result = divide(u,v)
+		self.assertEqual(result,2)
+
+	def test_8(self):
+		u = 25
+		v = 2
+		result = divide(u,v)
+		self.assertEqual(result,12.5)
 
 if __name__ == '__main__':
 	unittest.main()
